21d7472382f19ae0a04da0863fe069ba8a1bdfb4
Sejmou <samo.kolter@gmail.com>
2023-01-22 19:06:58 +0100

    fix scales

--- src/main.ts
+++ src/main.ts
@@ -37,14 +37,14 @@ json<MenuEntry[]>('menu.json').then(data => {
 
   const x = scaleBand()
     .domain(data.map(d => d.name))
-    .range([0, 500])
+    .range([0, graphWidth])
     .paddingInner(0.2) // space between bars
     .paddingOuter(0.1); // space between bars and edges of SVG
 
   const yValue = (d: MenuEntry) => d.orders;
   const y = scaleLinear()
     .domain([0, max(data, yValue)!])
-    .range([0, 500]);
+    .range([0, graphHeight]);
 
   // join data to rect elements in SVG (as SVG is empty, we will only get enter selections and no actual already existing DOM elements)
   const bars = graph.selectAll('rect').data(data);
